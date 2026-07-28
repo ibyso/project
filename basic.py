6afe8625bf4a79e665b851e72e36ef02a38bc58c
seoseungyeon <ssy6256a@gmail.com>
2026-07-28 12:06:45 +0900

    파일 불러와서 commit

--- basic.py
+++ basic.py
@@ -14,13 +14,3 @@ print(a)
 a = -4321.324
 print(a)
 
-multiline = """Life is too short,\nYou need python"""
-print(multiline)
-
-multiline = """
-Life is too short,
-You need python
-"""
-print(multiline)
-
-print("git hub commit")
