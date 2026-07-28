64dee1c5a07df9daaf9099801c49e1dec5c58f7e
seoseungyeon <ssy6256a@gmail.com>
2026-07-28 12:03:14 +0900

    main branch merge test yeon

---
+++ basic.py
@@ -0,0 +1,26 @@
+#자료형
+#숫자형, 문자형
+
+#1. 숫자형 - 정수, 실수 ,8진수, 16진수
+a = 123
+print(a)
+
+a = -1432
+print(a)
+
+a = 1.23
+print(a)
+
+a = -4321.324
+print(a)
+
+multiline = """Life is too short,\nYou need python"""
+print(multiline)
+
+multiline = """
+Life is too short,
+You need python
+"""
+print(multiline)
+
+print("git hub commit")
