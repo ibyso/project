09723b58d3dc244faf6fb2d2c87598dc631318ad
seoseungyeon <ssy6256a@gmail.com>
2026-07-28 11:59:59 +0900

    basic.py 충돌테스트 yeon

--- basic.py
+++ basic.py
@@ -2,16 +2,16 @@
 # 숫자형, 문자형
 
 # 1. 숫자형 - 정수, 실수, 8진수, 16진수
-a = 567
+a = 123
 print(f"양의 정수 : {a}")
 
-a = -890
+a = -43
 print(f"음의 정수 : {a}")
 
-a = 8.1
+a = 8.13
 print(f"양의 실수 : {a}")
 
-a = -5.5
+a = -5.53432
 print(f"음의 실수 : {a}")
 
 multiline = """life is too short,\nYou need Python"""
