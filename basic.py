a63c57ca443a8678d05da90844d5f15825e89309
seoseungyeon <ssy6256a@gmail.com>
2026-07-28 12:32:36 +0900

    basic 수정

--- basic.py
+++ basic.py
@@ -2,16 +2,16 @@
 # 숫자형, 문자형
 
 # 1. 숫자형 - 정수, 실수, 8진수, 16진수
-a = 123
+a = 123324
 print(f"양의 정수 : {a}")
 
-a = -43
+a = -43432
 print(f"음의 정수 : {a}")
 
-a = 8.13
+a = 8.1343
 print(f"양의 실수 : {a}")
 
-a = -5.53432
+a = -5.534324
 print(f"음의 실수 : {a}")
 
 multiline = """life is too short,\nYou need Python"""
@@ -22,4 +22,4 @@ life is too short,
 You need Python
 """
 print(multiline)
-print("heoun-충돌 테스트")
+print("yeon-충돌 테스트")
